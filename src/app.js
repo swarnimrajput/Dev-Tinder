8c023a762072e577b40bea99c4bc91345cafe8a5
swarnimrajput <swarnimrajput5826@gmail.com>
2024-11-16 13:40:19 +0530

    different routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
 const express= require("express");
 const app=express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello ");
+
+app.get("/test",(req,res)=>{
+    res.send("Hello from the get call");
 })
 
-app.use("/get",(req,res)=>{
-    res.send("Hello ");
+app.post("/test",(req,res)=>{
+    res.send("Hello from the post call");
+})
+
+app.delete("/test",(req,res)=>{
+    res.send("Deleted");
+})
+
+app.patch("/test",(req,res)=>{
+    res.send("Patch Call");
+})
+
+app.put("/test",(req,res)=>{
+    res.send({firstname:"Swarnim",lastname:"Rajput"})
 })
 
 app.use("/test",(req,res)=>{
     res.send("Hello From the Server");
 })
+
+
 
 app.listen(3000,()=>{
     console.log("Server conected successfully");
