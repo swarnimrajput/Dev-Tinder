50511b2cb2bb4cc50eb3982e062232f615bf5397
swarnimrajput <swarnimrajput5826@gmail.com>
2024-11-23 11:22:16 +0530

    error handling and try and catch

--- src/app.js
+++ src/app.js
@@ -2,20 +2,26 @@ const express= require("express");
 const app=express();
 
 const {adminAuth}=require("./middlewares/auth")
-
-app.use("/admin",adminAuth);
-
-app.get("/admin/addalluser",(req,res)=>{
-    
-        res.send("All data sent");
-    
-})
-
-app.get("/admin/deletealluser",(req,res)=>{
+// if there is any error we can always try to handle that using try and catch block
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something Went Wrong");
+    }
+   
   
-        res.send("deleted a user");
+})
+
+app.get("/getuserData",(req,res)=>{
+    try {
+        throw new Error("sfbsfbjbfb");
+    res.send("User data sent");
+    } catch (err) {
+        res.status(500).send("Some unexpected error happened");
+    }
     
 })
+
+
 
 app.listen(3000,()=>{
     console.log("Server conected successfully");
